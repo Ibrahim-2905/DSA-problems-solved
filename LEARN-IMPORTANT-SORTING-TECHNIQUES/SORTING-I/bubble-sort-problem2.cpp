db7e39121da51c7840116682bab5f018fc4b4e6f
pretender-2905 <pretender2905@gmail.com>
2026-07-01 19:49:29 +0500

    sorting practice

--- LEARN-IMPORTANT-SORTING-TECHNIQUES/SORTING-I/bubble-sort-problem2.cpp
+++ LEARN-IMPORTANT-SORTING-TECHNIQUES/SORTING-I/bubble-sort-problem2.cpp
@@ -35,3 +35,48 @@ int main(){
 
     return 0;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+// // again practice
+// // Bubble sort - Push maximum to the end by adjacent swap
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void bubble_sort(int arr[], int n){
+//     for(int i = n-1; i >= 1; i--){
+//         for(int j = 0; j <= i-1; j++){
+//             if(arr[j] > arr[j+1]){
+//                 // swap
+//                 int temp = arr[j];
+//                 arr[j] = arr[j+1];
+//                 arr[j+1] = temp; 
+//             };
+//         };
+//     };
+// };
+
+// int main(){
+//     int n;
+//     cout << "Enter a number";
+//     cin >> n;
+//     int arr[n];
+//     for(int i = 0; i < n; i++){
+//         cin >> arr[i];
+//     };
+//     bubble_sort(arr, n);
+//     cout << "The sorted array is" << endl;
+//     for(int i = 0; i < n; i++){
+//         cout << arr[i] << " ";
+//     };
+// };
--- LEARN-IMPORTANT-SORTING-TECHNIQUES/SORTING-I/selesction_sort_problem1.cpp
+++ LEARN-IMPORTANT-SORTING-TECHNIQUES/SORTING-I/selesction_sort_problem1.cpp
@@ -37,3 +37,51 @@ int main(){
 
     return 0;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+// again practice
+// find minimum and swap - selection sort
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void selection_sort(int arr[], int n){
+//     for(int i = 0; i <= n-2; i++){
+//         int mini = i;
+//         for(int j = i; j <= n-1; j++){
+//             if(arr[j] < arr[mini]){
+//                 mini = j;
+//             };
+//         };
+//         //swap 
+//         int temp = arr[mini];
+//         arr[mini] = arr[i];
+//         arr[i] = temp;
+//     };
+// };
+
+// int main(){
+//     int n;
+//     cout << "Enter a number" << endl;
+//     cin >> n;
+//     int arr[n];
+//     cout << "Enter an array" << endl;
+//     for (int i= 0; i < n; i++){
+//         cin >> arr[i];
+//     };
+//     selection_sort(arr, n);
+//     cout << "The sorted array is:" << endl;
+//     for(int i= 0; i< n; i++){
+//         cout << arr[i] << " " << endl;
+//     };
+// };
